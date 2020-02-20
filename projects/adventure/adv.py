5079ad0784bb8b2c0440176dafc74c10fbf1eaf5
ren.meta@gmail.com <ren.meta@gmail.com>
2020-02-20 18:40:37 -0500

    bread first search method

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,40 +5,88 @@ from world import World
 import random
 from ast import literal_eval
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
 # Load world
 world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
-
+print(f"RoomGraph: {room_graph[0]}")
 # Print an ASCII map
 world.print_rooms()
 
 player = Player(world.starting_room)
-
+print(player.current_room.get_exits())
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+
+def bfs(starting_room, destination):
+  queue = Queue()
+  player.current_room.id = starting_room
+  queue.enqueue([starting_room])
+  visited_rooms = set()
+  while queue.size() > 0:
+    path = queue.dequeue()
+    room_id = path[-1]
+    if room_id not in visited_rooms:
+      if room_id == destination:
+        return path
+      else:
+        for direction in room_graph:
+          if room_graph[direction] != "?":
+            new_path = path.copy()
+            new_path.append(room_graph[direction])
+            queue.enqueue(new_path)
+      visited_rooms.add(room_id)
+    return []
+bfs(0,2)
+
 
 
 
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
+# print(player.)
 visited_rooms.add(player.current_room)
 
-for move in traversal_path:
-    player.travel(move)
-    visited_rooms.add(player.current_room)
+# for move in traversal_path:
+#     player.travel(move)
+#     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
     print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
@@ -51,12 +99,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
