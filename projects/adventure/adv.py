0a4687767a278c46b2455678712814531395c344
ren.meta@gmail.com <ren.meta@gmail.com>
2020-02-20 19:46:44 -0500

    WIP traversing through each room in graph but stuck in infinite loop

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -35,11 +35,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
+# map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -49,7 +49,7 @@ print(f"RoomGraph: {room_graph[0]}")
 world.print_rooms()
 
 player = Player(world.starting_room)
-print(player.current_room.get_exits())
+# print(player.current_room.get_exits())
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
@@ -67,6 +67,7 @@ def bfs(starting_room, destination):
         return path
       else:
         for direction in room_graph:
+        #   print(f"Room graph {room_graph[direction]}")  
           if room_graph[direction] != "?":
             new_path = path.copy()
             new_path.append(room_graph[direction])
@@ -74,8 +75,49 @@ def bfs(starting_room, destination):
       visited_rooms.add(room_id)
     return []
 bfs(0,2)
-
-
+inverse_directions = {"n": "s", "s": "n", "e": "w", "w": "e"}
+prev_move = None
+while True:
+    # print(player.current_room)
+    current_exits = player.current_room.get_exits()
+    room_id = player.current_room.id
+    target = bfs(player.current_room.id, 3)
+    print(f'Traversing to target...')
+    for direction in current_exits:
+      if room_id == target:
+        next_move = direction
+        break
+    else:
+        directions = []
+        for direction in current_exits:
+            # If adjacent room_id not visited yet, add that direction
+            if room_id == "?":
+                directions.append(direction)
+        if len(directions) == 0:
+            print('All adjacent rooms visited..')
+            target = bfs(room_id, "?")
+            for direction in current_exits:
+                if room_id == target:
+                    next_move = direction
+                    break
+            directions = [direction for direction in current_exits]
+            if len(directions) == 1:
+                next_move = directions[0]
+            else:
+                if prev_move is not None:
+                    directions.remove(inverse_directions[prev_move])
+                next_move = random.choice(directions)
+        else:
+            next_move = random.choice(directions)
+        print(f'Next room: {player.travel([next_move])}')
+        print(f'Room [{room_id}] to Room [{player.travel([next_move])}]')
+        if player.travel([next_move]) == "?":
+            print(f"Traveling to the unknown.. ({len(room_graph)}/500)")
+            end_room = player.travel(next_move)
+        else:
+            end_room = player.travel([next_move])
+            prev_move = next_move
+            current = end_room
 
 
 # TRAVERSAL TEST
